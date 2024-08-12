099233cb76681da612825f1713498eb9935d3fe8
Wasanjith <wasakodi79@gmail.com>
2024-08-12 20:41:43 +0530

    On branch main  Your branch is up to date with 'origin/main'.

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
+import Food from "./Food.jsx"
 
 function App() {
   return(
     <>
       <Header/>
+      <Food/>
+      <Food/>
+      <Food/>
+      <Food/>
+      <Food/>
       <Footer/>
     </>
     
